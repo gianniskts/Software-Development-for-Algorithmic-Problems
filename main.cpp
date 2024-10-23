f0b86dc4d97ee7ae9e9acff4bbc9b009683e6141
DIalexalatzas <alexalatzas7@gmail.com>
2024-10-24 00:56:08 +0300

    update main, update readme

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #include "../includes/PolygonManipulation.h"
 #include "../includes/ActionFunctions.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
 
         Polygon_2 result;
 
+        std::string json_file = argv[1];
+
         // Parse the input file
-        InputJSON<int> input_json = parse_file<int>("../input.json");
+        InputJSON<int> input_json = parse_file<int>(json_file);
 
         // Perform constrained delaunay triangulation
         result = delaunay_const_triangulation<int>(input_json);
